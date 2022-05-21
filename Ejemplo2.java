a1fb0b8c321a9dfead549a3c1ade12df17f9bf7b
selvinguezgiron <105984009+selvinguezgiron@users.noreply.github.com>
2022-05-21 10:01:53 -0600

    Create Ejemplo2.java

---
+++ Ejemplo2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {  
+  public static void main(String args[]) { 
+    Scanner leer = new Scanner(System.in);
+    System.out.println("Ingrese su nombre: ");
+    String nombre = leer.next();
+    System.out.println("Bienvenido "+nombre);
+    
+  } 
+}
